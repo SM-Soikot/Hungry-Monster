5062096d2ff5f046ad88cbc9115303a40cb725b0
SM-Soikot <smsoikot123@gmail.com>
2021-02-10 00:02:41 +0600

    first commit

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
-fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-.then(res => res.json())
-.then(data => food(data))
+const findFood = () => {
+    const search = document.getElementById('searchBar').value;
+    const url = `https://www.themealdb.com/api/json/v1/1/categories.php`
 
-const food = ingredients => {
- for(let i = 0; i < ingredients.length; i++){
-     const ingredient = ingredients [i];
-     console.log(ingredient.name)
- }
+    fetch(url)
+        .then(res => res.json())
+        .then(data => ShowFoods(data.categories))
 }
+const ShowFoods = food => {
+    const foodCorner = document.getElementById('food-corner');
+
+    food.forEach(foods => {
+        const foodDiv = document.createElement('div');
+        foodDiv.className = 'col card-size';
+        foodDiv.innerHTML = `<div class="col card-size">
+        <div class="card h-100">
+        <img src="${foods.strCategoryThumb}" class="card-img-top image" alt="..."></img>
+        <div class="card-body">
+        <button onclick="getIngredients('${foods.detailsdetails}')" class="card-title button">${foods.strCategory}</button>
+        </div>  </div>
+        </div>`
+        foodCorner.appendChild(foodDiv);
+    });
+}
+const getIngredients = details => {
+c
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => showIngredients(data.meals))
+    
+}
+const showIngredients = ingredients => {
+    const ingredientDiv = document.getElementById('ingredients');
+    ingredientDiv.innerHTML = `
+    <h3>${ingredients.details}</h3>
+    <li>${ingredients.strInstructions}</li>
+    `
+   
+}
+
